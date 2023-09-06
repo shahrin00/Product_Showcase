9edf05a70be192194bf7fff17f7ee2aa970bd6b0
Shahrin Islam <shahrinislam959@gmail.com>
2023-09-06 18:46:52 +0600

    check login button

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signInHandler = () => {
+    console.log(email, password);
+  };
+
   return (
     <div className="flex justify-center">
-        <div className=" login_form mt-8 bg-blue-200" >
-      <div className="">
-      <label htmlFor="">Enter Your Email:</label>
-        <input type="email" placeholder="Enter your email" className="p-2 rounded" /> <br />
-        
-      </div>
-      <div className="">
-      <label htmlFor="">Enter Your password:</label>
-            <input type="password" placeholder="Enter Password" className="p-2 rounded" />
-
+      <div className=" login_form mt-8 bg-blue-200">
+        <div className="">
+          <label htmlFor="">Enter Your Email:</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Enter your email"
+            className="p-2 rounded"
+          />{" "}
+          <br />
         </div>
-        <button className="login_btn  bg-cyan-300  mt-2 w-[100px] rounded h-[50px]">Login</button>
-
+        <div className="">
+          <label htmlFor="">Enter Your password:</label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Enter Password"
+            className="p-2 rounded"
+          />
+        </div>
+        <button
+          onClick={signInHandler}
+          className="login_btn  bg-cyan-300  mt-2 w-[100px] rounded h-[50px]"
+        >
+          Login
+        </button>
+      </div>
     </div>
-    
-    </div>
-
   );
 };
 
